0846b46723bbfcdaa5a778bb376239b116fb5e09
ivankossdev <ivan.koss@yandex.ru>
2024-12-25 13:10:35 +0300

    MatrixSpiSendWord func

--- 16f876_matr_8x8_digital.X/main.c
+++ 16f876_matr_8x8_digital.X/main.c
@@ -17,15 +17,17 @@
  * pin 14 RC3      -> CLK               *
  * pin 16 RC5      -> DIN               *
  *******************END******************/
+void MatrixSpiSendWord(int segment, char data){
+    SpiSendByte((char)segment);
+    SpiSendByte(data);
+}
 
 void MatrixSendTest(int segment, char data, int c,  char data2) {
     cs = 0;
-    SpiSendByte((char)segment);
-    SpiSendByte(data);
+    MatrixSpiSendWord((char)segment, data);
     int i = c;
     do{
-        SpiSendByte((char)segment);
-        SpiSendByte( data2);
+        MatrixSpiSendWord((char)segment, data2);
     }while(--c);
     cs = 1;
 }
@@ -34,20 +36,14 @@ void main(void) {
     SpiInit();
     MatrixInit();
     PortBInit();
-//    TCoord shapePosition; 
-    char * pSimvol = (char *)calloc(8, sizeof(char));
-    char * pSimvol2 = (char *)calloc(8, sizeof(char));
-//    char Display[8] = {'\0'};
-//    bool bR = true;
     
-    while(1){
-//        SetPosition(&shapePosition, 0, 0); 
+    while(1){ 
 
         for(int c = 0, d = 9 ; c < 10; c++, d--){
-            pSimvol = GetDgigtal(c);
-            pSimvol2 = GetDgigtal(d);
+//            pSimvol = GetDgigtal(c);
+//            pSimvol2 = GetDgigtal(d);
             for(int i = 0; i < 8; i++){
-                MatrixSendTest(i + 1,pSimvol[i], 1, pSimvol2[i]);
+                MatrixSendTest(i + 1,GetDgigtal(c)[i], 1, GetDgigtal(d)[i]);
             }
             __delay_ms(500);
         }
@@ -57,22 +53,6 @@ void main(void) {
         MatrixClear();
         __delay_ms(1000);
         
-//        for(int i = 0; i < 10; i++){
-//            
-//            if(bR){
-//                pSimvol = GetDgigtal(i);
-//                ShowShape(&shapePosition, 8, pSimvol);
-//                __delay_ms(250); 
-//            }
-//            else{
-//                pSimvol = GetDgigtal(i);
-//                Rotate(pSimvol, Display, 8);
-//                ShowShape(&shapePosition, 8, Display);
-//                __delay_ms(250); 
-//            }
-//        }
-//        if(bR) bR = false;
-//        else bR = true;
     }    
     return;
 }
